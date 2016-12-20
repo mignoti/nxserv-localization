4c106691b6f20bb1d43505d4920b1cfd79d5f09d
Sergei Ch <noiwex1911@gmail.com>
2016-12-20 12:17:41 +0300

    nxshop locale

---
+++ all/nxshop/locale.lua
@@ -0,0 +1,206 @@
+nxshop.langs = {
+	en = {
+		subscriptions = "Subscriptions & Specials",
+		weapons = "Weapons",
+		cars = "Cars",
+		hats = "Appearance",
+		bought = "Purchased",
+		buying_period = "Valid for %s",
+		expires_in = "Expires in ",
+		expired = "Expired!",
+		discount = "%s, %d%% OFF!",
+		open_in_browser = "To get required information, copy this link and open it in your favorite browser",
+		copy_link = "Copy link",
+		nofunds_ask = "Your account doesn't have enough funds to buy this item.\nWould you like to top up your balance?",
+		nofunds = "Insufficient funds",
+		yes = "Yes",
+		no = "No",
+		sending_request = "Sending request...",
+		item_bought = "Success: Item Purchased",
+		error = "Error",
+		button_topup = "How to top up?",
+		scroll = "Scroll with mouse wheel",
+		already_bought = "You already own this item",
+		few_seconds = "few seconds",
+		seconds = " seconds",
+		minutes = " minutes",
+		hours = " hours",
+		days = " days",
+		you_bought = "You bought ",
+		you_already_own = "You already own this item!",
+		no_item = "There's an error in processing your request.",
+		thanks = "Your funds have been added to your account, cheers!",
+		buy_for = "Buy for ",
+		premium_cars = "Premium Cars",
+		regular_cars = "Regular cars",
+		slots = "Slots:",
+		store = "Store:",
+		ui_back = "< Back",
+		game_money = "In-Game Money: ",
+		darkrp_premium_desc = [["Premium" package allows you to gain additional advantages in the game on our DarkRP servers.
+With "Premium" you gain money quickier and survive better in the city full of criminality.
+
+Character:
+* Respawn time shortens to 5 seconds
+* Hunger rate is 2 times slower
+* Max armor increases up until 200%
+* You get access to stylish customizable models on jobs like Bartender, City Mayor (Breen), Fishman (Eli), Mob Boss, Medic (Kleiner), Gun Dealer (Father Grigory), Security Guard (Barney), Taxi Driver (Barney)
+* Premium icon in scoreboard makes you more noticeable
+
+Cars:
+* Cars consume 50% less fuel
+* 10% off when buying cars
+* Car renting becomes available
+* Cosmetic tuning becomes available (spoilers, bumpers, skirts, hoods and etc)
+
+Gameplay:
+* You can initiate demote votes
+* Job limits aren't applied to you, you can get any job at any time
+* When starting (as Mayor) a lottery you get 10% from lottery pool
+* Money Printers and Fishing give 50% more income]],
+		richbitch_desc = "Only for DarkRP. Number of purchases is unlimited.",
+		richbitch_title = "In-Game Currency %s",
+		premium_car_desc_1 = [[Exclusive Premium-class vehicle, permanently added to your vehicle list.
+	%d passenger seat.]],
+		premium_car_desc_2 = [[Exclusive Premium-class vehicle, permanently added to your vehicle list.
+	%d passenger seats.]],
+	},
+
+	ru = {
+		subscriptions = "Подписки и Спецпредложения",
+		weapons = "Оружие",
+		cars = "Машины",
+		hats = "Одежда",
+		bought = "Приобретено",
+		buying_period = "Приобретается на %s",
+		expires_in = "Истекает через ",
+		expired = "Действие заканчивается",
+		discount = "%s, Скидка %d%%!",
+		open_in_browser = "Для получения инструкции по пополнению счёта, скопируйте эту ссылку и откройте её в вашем браузере.\nЧтобы свернуть окно с игрой, нажмите Win (флажок) и D одновременно.",
+		copy_link = "Скопировать ссылку",
+		nofunds_ask = "На вашем счету недостаточно средств для приобретения данного предмета.\nЖелаете пополнить баланс?",
+		nofunds = "Недостаточно средств",
+		yes = "Да",
+		no = "Нет",
+		sending_request = "Передача данных...",
+		item_bought = "Предмет куплен",
+		error = "Ошибка",
+		button_topup = "Как пополнить?",
+		scroll = "Прокрутка колёсиком мыши",
+		already_bought = "Предмет уже куплен",
+		few_seconds = "пару секунд",
+		seconds = " с",
+		minutes = " мин",
+		hours = " ч",
+		days = " дн",
+		you_bought = "Вы приобрели ",
+		you_already_own = "У вас уже имеется данный предмет!",
+		no_item = "Ошибка при поиске предмета.",
+		thanks = "Ваши средства поступили на счёт, счастливых покупок!",
+		buy_for = "Купить за ",
+		premium_cars = "Премиум машины",
+		regular_cars = "Обычные машины",
+		slots = "Слоты:",
+		store = "Магазин:",
+		ui_back = "< Назад",
+		game_money = "Игровые деньги: ",
+		darkrp_premium_desc = [[Пакет "Премиум" даст вам дополнительные преимущества в игре на наших DarkRP серверах.
+С "Премиум" вы зарабатываете быстрее и выживаете чаще в городе с высокой преступностью.
+
+Персонаж:
+* Время ожидания возрождения сокращается до 5 секунд
+* Голод накапливается в 2 раза медленнее
+* Лимит брони увеличивается до 200%
+* Появляется доступ к красивым изменяемым моделям на профессиях Бармен, Глава города (Брин), Рыбак (Илай), Глава банды, Медик (Кляйнер), Продавец оружия (Отец Григорий), Охранник, Таксист
+* Премиум иконка в скорборде заметно выделяет вас среди остальных
+
+Машины:
+* Автомобили потребляют на 50% меньше топлива
+* Выдаётся скидка 10% на приобретение транспорта
+* Появляется доступ к аренде автомобилей
+* Появляется опция изменения косметических частей на автомобилях (спойлеры, бамперы и прочее)
+
+Геймплей:
+* Даётся доступ к голосованиям на увольнение
+* Занимать профессии можно в обход лимитов
+* Когда вы играете за Главу города и проводите лотерею, вы получаете 10% от выигрыша
+* Принтеры и рыбалка дают на 50% больше прибыли]],
+		richbitch_desc = "Только для DarkRP. Количество приобретений неограниченно.",
+		richbitch_title = "Игровая валюта %s",
+		premium_car_desc_1 = [[Эксклюзивный автомобиль Премиум-класса, добавляемый в список купленных машин навсегда.
+	%d пассажирское сиденье.]],
+		premium_car_desc_2 = [[Эксклюзивный автомобиль Премиум-класса, добавляемый в список купленных машин навсегда.
+	%d пассажирское сиденья.]],
+		premium_car_desc_3 = [[Эксклюзивный автомобиль Премиум-класса, добавляемый в список купленных машин навсегда.
+	%d пассажирское сиденья.]],
+	},
+
+	fr = {
+		subscriptions = "Abonnements & Offres Spéciales",
+		weapons = "Armes",
+		cars = "Voitures",
+		hats = "Apparence",
+		bought = "Acheté",
+		buying_period = "Valable pour %s",
+		expires_in = "Expire dans ",
+		expired = "Expiré!",
+		discount = "%s, %d%% de rabais!",
+		open_in_browser = "Pour obtenir les informations requises, copie ce lien et l'ouvrir dans votre navigateur préféré",
+		copy_link = "Copier le lien",
+		nofunds_ask = "Votre compte ne dispose pas de suffisamment de fonds pour acheter ce produit.\nSouhaitez recharger votre solde?",
+		nofunds = "L'insuffisance des fonds",
+		yes = "Oui",
+		no = "Non",
+		sending_request = "Demande d'envoi...",
+		item_bought = "Succès!",
+		error = "Erreur",
+		button_topup = "Comment recharger?",
+		scroll = "Molette de la souris – rotation",
+		already_bought = "Vous possédez déjà cet élément",
+		few_seconds = "quelques secondes",
+		seconds = " secondes",
+		minutes = " minutes",
+		hours = " heures",
+		days = " jours",
+		you_bought = "Vous avez acheté ",
+		you_already_own = "Vous possédez déjà cet élément!",
+		no_item = "Il y a une erreur dans le traitement de votre demande.",
+		thanks = "Vos fonds ont été ajoutés à votre compte, merci!",
+		buy_for = "Acheter pour ",
+		premium_cars = "Premium Voitures",
+		regular_cars = "Voitures ordinaires",
+		slots = "Fentes:",
+		store = "Store:",
+		ui_back = "< Retour",
+		game_money = "En Jeu De L'Argent: ",
+		darkrp_premium_desc = [["Premium" vous donne des avantages supplémentaires dans le jeu sur notre DarkRP serveurs.
+Avec "Premium", vous gagnez de l'argent quickier et de mieux survivre dans la ville est pleine de la criminalité.
+
+Personnage:
+* Le temps de respawn réduit à 5 secondes
+* La faim taux est 2 fois plus lent
+* Max armure augmente jusqu'à 200%
+* Vous obtenez l'accès à l'élégant personnalisable modèles sur l'emploi comme Bartender, City Mayor (Breen), Fishman (Eli), Mob Boss, Medic (Kleiner), Gun Dealer (Father Grigory), Security Guard (Barney), Taxi Driver (Barney)
+* Premium icône dans le tableau de bord vous rend plus perceptible
+
+Voitures:
+* Les voitures consomment 50% moins de carburant
+* 10% de rabais lors de l'achat de voitures
+* Voiture de location devient disponible
+* Cosmétiques tuning devient disponible (spoilers, pare-chocs, jupes, hottes, etc.)
+
+Gameplay:
+* Vous pouvez lancer rétrograder votes
+* Emploi limites ne s'appliquent pas à vous, vous pouvez obtenir n'importe quel travail à n'importe quel moment
+* En tant que maire, lors du démarrage d'une loterie, vous bénéficiez de 10% de la loterie de la piscine
+* L'argent des Imprimantes et de la Pêche donner 50% plus de revenus]],
+		richbitch_desc = "Only for DarkRP. Number of purchases is unlimited.",
+		richbitch_title = "In-Game Currency %s",
+		premium_car_desc_1 = [[Exclusive Premium-class vehicle, permanently added to your vehicle list.
+	%d passenger seat.]],
+		premium_car_desc_2 = [[Exclusive Premium-class vehicle, permanently added to your vehicle list.
+	%d passenger seats.]],
+	},
+}
+
+nxshop.l = nxserv.region_shop == "ru" and nxshop.langs.ru or nxshop.langs.en
