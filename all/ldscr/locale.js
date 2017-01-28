cc4de8aec5a059ade78fc00728feb4e09815cae3
Sergei Ch <noiwex1911@gmail.com>
2017-01-28 16:05:52 +0300

    ldscr

---
+++ all/ldscr/locale.js
@@ -0,0 +1,65 @@
+var langs = {
+	ru: {
+		init: "Загрузка",
+		mounting: "Workshop аддоны: подключаем",
+		ws_done: "Workshop аддоны: готово",
+		sending_info: "Почти готово",
+		ws_adds: "Workshop аддоны: ",
+		checking: "Проверяем ",
+		unpacking: "Распаковываем ",
+		loading: "Загружаем ",
+		fastdl: "FastDL файлы: ",
+		bsp: "Полоса загрузки может стоять на месте или возвращаться назад - это нормально",
+		resources: "Подгружаем ресурсы",
+		hints: [
+			"Если проверка Workshop аддонов идёт необычно очень долго, то закройте GMod и перезагрузите Steam.",
+			"Если произошел креш, не волнуйтесь: сервер быстро перезагрузится, а вещи, профессия и местоположение будут вам возвращены автоматически.",
+			"Обязательно ознакомьтесь с правилами, а также читайте новости. В них нередко содержится важная информация.",
+			"Игра на серверах NxServ абсолютно бесплатна и требует лишь лицензионную копию Garry's Mod, однако вы можете порадовать себя специальными предметами в нашем магазине. Для некоторых режимов вроде NxRP (бывш. DarkRP) возможна покупка Премиум-подписки на месяц. Премиум сделает вашу игру приятной и удобной.",
+			'На всех серверах NxServ доступны 100% наши, с любовью сделанные аксессуары для вашего игрового персонажа. Ознакомиться с ассортиментом магазина можно в меню F4 (для DarkRP) или "Магазин" в ESC-меню.',
+			"При постройке старайтесь использовать наименьшее количество объектов: вместо нескольких маленьких пропов возьмите один большой.",
+			"Если вашей постройке из дупликатора не нужно двигаться (дом, магазин, забор и т.д.), загружайте ее с отмеченной галкой \"Paste w/o constraints\". Это уберет соединения и заморозит постройку, благодаря чему на сервер не будет лишней нагрузки от того, что все равно не должно двигаться.",
+		]
+	},
+	en: {
+		init: "Loading",
+		mounting: "Workshop Addons: Mounting",
+		ws_done: "Workshop Addons: Ready",
+		sending_info: "Almost there...",
+		ws_adds: "Workshop Addons: ",
+		checking: "Checking ",
+		unpacking: "Unpacking ",
+		loading: "Downloading ",
+		fastdl: "FastDL Files: ",
+		bsp: "Loading bar can stand still or reset - don't worry, it is fine",
+		resources: "Loading resources",
+		hints: [
+			"If Workshop addons check goes too long, then close your GMod and restart Steam.",
+			"Possible jobs aren't limited by list in F4 menu. As Citizen, you may think of your own occupation. Say, cinema owner or goods reseller.",
+			"Don't worry about server crashes - it will reload in short time and you'll get all of your items, your job and location restored automatically.",
+			"Be sure to check the rules and game guide. You can also read our news - there's always something interesting about upcoming updates and more.",
+		]
+	},
+	fr: {
+		init: "Chargement en cours...",
+		mounting: "Workshop Addons: Montage",
+		ws_done: "Workshop Addons: Complété",
+		sending_info: "Presque là...",
+		ws_adds: "Workshop Addons: ",
+		checking: "Vérification ",
+		unpacking: "Déballage ",
+		loading: "Téléchargement ",
+		fastdl: "FastDL Files: ",
+		bsp: "Barre de chargement peut rester immobile ou réinitialiser - ne vous inquiétez pas, il est très bien",
+		resources: "Chargement des ressources",
+		hints: [
+			"Si \"Workshop Addons\" vérifier va trop long, puis fermez votre GMod et redémarrez Steam.",
+			"Emplois possibles ne sont pas limités par la liste dans le menu F4. Comme citoyen, vous pouvez penser à votre propre profession. Dites, propriétaire du cinéma ou de biens détaillant.",
+			"Ne vous inquiétez pas les pannes de serveur - il se rechargera en peu de temps et vous aurez tous vos articles, votre travail et l'emplacement restauré automatiquement.",
+			"Assurez-vous de vérifier les règles et guide de jeu. Vous pouvez également lire nos nouvelles - il y a toujours quelque chose d'intéressant au sujet de prochaines mises à jour et plus.",
+			"It is absolutely free to play on NxServ servers, the only thing you need is a licensed copy of Garry's Mod, however you can please yourself with special items in our shop. For some gamemodes like NxRP (former DarkRP) it is possible to purchase a Premium subscription for one month. Premium makes your game pleasant, easy and comfortable.",
+			"We've made lovely accessories for your in-game character, which you can wear on any of our servers. To see whole range of available items, use F4 menu (on DarkRP) or \"Shop\" in the ESC-menu.",
+			//"Savez-vous l'anglais? Aidez-nous à traduire notre serveur vers le français. Visitez https://nxserv.eu/fr",
+		]
+	}
+}
